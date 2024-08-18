1be84d0daa6b9dc517e5b5b09efd11e925c93df4
demi <phandiem0709@gmail.com>
2024-08-19 00:14:17 +0700

    #3 - design login form again with picture and change the background color

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -8,6 +8,7 @@ import {
   Typography,
   IconButton,
   InputAdornment,
+  Grid,
 } from "@mui/material";
 import { Visibility, VisibilityOff } from "@mui/icons-material";
 import { Link } from "react-router-dom";
@@ -24,94 +25,118 @@ function LoginPage() {
   };
 
   return (
-    <Card
+    <div
       style={{
-        maxWidth: 500,
-        margin: "auto",
-        padding: "20px",
-        marginTop: "200px",
-        backgroundColor: "#b7e4c7",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100vh",
+        backgroundColor: "#f0fff1",
       }}
     >
-      <CardHeader
-        title="Login Page"
-        titleTypographyProps={{
-          variant: "h5",
-          align: "center",
-          style: { color: "#1b4332" },
+      <Card
+        style={{
+          width: "1000px",
+          height: "600px",
+          backgroundColor: "#b7e4c7",
+          borderRadius: "30px",
         }}
-      />
-      <CardContent>
-        <form>
-          <div style={{ marginBottom: "20px" }}>
-            <TextField
-              label="User Name"
-              name="username"
-              fullWidth
-              required
-              variant="outlined"
-              placeholder="Enter your username"
-              sx={{
-                "& label": { color: "#1b4332" },
-              }}
-              InputProps={{
-                style: { backgroundColor: "#d8f3dc", color: "#1b4332" },
+      >
+        <Grid container spacing={0} style={{ height: "100%" }}>
+          <Grid item xs={5}>
+            <img
+              src="https://plus.unsplash.com/premium_photo-1679870686665-a1b0b6ca56a9?q=80&w=1780&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+              alt="Login Illustration"
+              style={{
+                width: "100%",
+                height: "100%",
+                objectFit: "cover",
               }}
             />
-          </div>
-
-          <div style={{ marginBottom: "20px" }}>
-            <TextField
-              label="Password"
-              name="password"
-              fullWidth
-              required
-              variant="outlined"
-              type={showPassword ? "text" : "password"}
-              placeholder="Enter your password"
-              sx={{
-                "& label": { color: "#1b4332" },
-              }}
-              InputProps={{
-                style: { backgroundColor: "#d8f3dc", color: "#1b4332" },
-                endAdornment: (
-                  <InputAdornment position="end">
-                    <IconButton
-                      onClick={handleClickShowPassword}
-                      onMouseDown={handleMouseDownPassword}
-                    >
-                      {showPassword ? <VisibilityOff /> : <Visibility />}
-                    </IconButton>
-                  </InputAdornment>
-                ),
+          </Grid>
+          <Grid item xs={7}>
+            <CardHeader
+              title="Login Page"
+              titleTypographyProps={{
+                variant: "h5",
+                align: "center",
+                style: { color: "#1b4332" },
               }}
             />
-          </div>
+            <CardContent>
+              <form>
+                <div style={{ marginBottom: "20px" }}>
+                  <TextField
+                    label="User Name"
+                    name="username"
+                    fullWidth
+                    required
+                    variant="outlined"
+                    placeholder="Enter your username"
+                    sx={{
+                      "& label": { color: "#1b4332" },
+                    }}
+                    InputProps={{
+                      style: { backgroundColor: "#d8f3dc", color: "#1b4332" },
+                    }}
+                  />
+                </div>
 
-          <Typography variant="body2" align="right">
-            <Link
-              to="/forgot-password"
-              style={{ textDecoration: "none", color: "#1b4332" }}
-            >
-              Forgot Password?
-            </Link>
-          </Typography>
+                <div style={{ marginBottom: "20px" }}>
+                  <TextField
+                    label="Password"
+                    name="password"
+                    fullWidth
+                    required
+                    variant="outlined"
+                    type={showPassword ? "text" : "password"}
+                    placeholder="Enter your password"
+                    sx={{
+                      "& label": { color: "#1b4332" },
+                    }}
+                    InputProps={{
+                      style: { backgroundColor: "#d8f3dc", color: "#1b4332" },
+                      endAdornment: (
+                        <InputAdornment position="end">
+                          <IconButton
+                            onClick={handleClickShowPassword}
+                            onMouseDown={handleMouseDownPassword}
+                          >
+                            {showPassword ? <VisibilityOff /> : <Visibility />}
+                          </IconButton>
+                        </InputAdornment>
+                      ),
+                    }}
+                  />
+                </div>
 
-          <Button
-            type="submit"
-            variant="contained"
-            fullWidth
-            style={{
-              marginTop: "20px",
-              backgroundColor: "#40916c",
-              color: "#fff",
-            }}
-          >
-            Login
-          </Button>
-        </form>
-      </CardContent>
-    </Card>
+                <Typography variant="body2" align="right">
+                  <Link
+                    to="/forgot-password"
+                    style={{ textDecoration: "none", color: "#1b4332" }}
+                  >
+                    Forgot Password?
+                  </Link>
+                </Typography>
+
+                <Button
+                  type="submit"
+                  variant="contained"
+                  fullWidth
+                  style={{
+                    marginTop: "20px",
+                    backgroundColor: "#40916c",
+                    color: "#fff",
+                  }}
+                >
+                  Login
+                </Button>
+              </form>
+            </CardContent>
+          </Grid>
+        </Grid>
+      </Card>
+    </div>
   );
 }
 
