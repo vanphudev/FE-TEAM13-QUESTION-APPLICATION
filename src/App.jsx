36d020c1c9e7b059163208993e2893d29d5e211c
Loc Dang <locdpxse182614@fpt.edu.vn>
2024-08-19 15:26:17 +0700

    #20 - Footer new

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import "./styles/index.scss";
-import {BrowserRouter} from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import RouterPublic from "./routes/publicRoute.jsx";
 import Data from "./resources/data.js";
 import Header from "./layouts/header.jsx";
-
+import Footer from "./components/Footer/Footer.jsx";
 const App = () => {
    document.title = Data.title;
    return (
@@ -14,6 +14,7 @@ const App = () => {
             {/* Menu Option */}
             <RouterPublic />
             {/* Footer  */}
+            <Footer />
          </BrowserRouter>
       </>
    );
